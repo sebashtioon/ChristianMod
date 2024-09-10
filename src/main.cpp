a9319e596c54de3a8bbcf4943b3e464d7f0d2949
sebashtioon <sebastiansuciu607@gmail.com>
2024-09-11 07:44:36 +1000

    change happy btn sprite scale (0.1)

--- src/main.cpp
+++ src/main.cpp
@@ -59,6 +59,7 @@ protected:
 /* -------------------------------------------------------------------------- */
 /* ---------------------------------- Happy --------------------------------- */
         auto happyBtnSprite = CCSprite::create("happy-complete.png"_spr);
+        happyBtnSprite->setScale(0.1f);
         auto happyBtn = CCMenuItemSpriteExtra::create(
             happyBtnSprite, 
             this, 
@@ -79,7 +80,7 @@ protected:
         calmBtn->setPosition({-42.f, 47.f});
 /* ------------------- Add buttons to the hayftButtonsMenu ------------------ */
         hayftButtonsMenu->addChild(hayftPopup_InfoButton);
-        
+
         hayftButtonsMenu->addChild(happyBtn);
         hayftButtonsMenu->addChild(calmBtn);
   
